7ddf9a8ff9eb1017122a43e51efa2e9e102e7789
king <3925@winnermedical.com>
2022-03-02 10:59:24 +0800

    接入了 flutter_winner_app

---
+++ lib/common/app_conifg.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_winner_app/flutter_winner_app.dart';
+
+class AppConfog extends WinnerAppConfig {
+  @override
+  Widget appHome(BuildContext context) {
+    throw Container(
+      color: Colors.red,
+    );
+  }
+
+  @override
+  WinnerEnvironmentUrl get environmentUrl {
+    return WinnerEnvironmentUrl(
+      debug: "http://127.0.0.1",
+      profile: "http://10.10.57.133",
+      release: "http://127.0.0.1",
+    );
+  }
+
+  @override
+  SentryHost get sentryHost {
+    return SentryHost(
+      debug:
+          "https://c20774016ffa4879a7ad450824d577f9@o1052791.ingest.sentry.io/6238242",
+      release:
+          "https://01003455d0eb4963bf49a18d7de508ca@o1052791.ingest.sentry.io/6238244",
+    );
+  }
+}
